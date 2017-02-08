8a5684434c48a16cb06c018c9201a11e99fd4282
Liu Xiaoyi <circuitcoder0@gmail.com>
2017-02-08 20:59:58 +0800

    Added links to Bilibili & GitHub. Updated styles.

--- action.js
+++ action.js
@@ -5,3 +5,11 @@ function load() {
 function gotoBlog() {
   window.location.href = "//meow.c-3.io";
 }
+
+function gotoGitHub() {
+  window.location.href = "//github.com/CircuitCoder";
+}
+
+function gotoBilibili() {
+  window.location.href = "//space.bilibili.com/523981";
+}
