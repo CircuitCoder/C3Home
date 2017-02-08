43c464633ed255b8428b1563f1403b4b4da88ac1
Liu Xiaoyi <circuitcoder0@gmail.com>
2017-02-08 21:03:45 +0800

    Using anchors instead of javascript functions for links

--- action.js
+++ action.js
@@ -1,15 +1,3 @@
 function load() {
   document.querySelector("body").classList.add("visible");
 }
-
-function gotoBlog() {
-  window.location.href = "//meow.c-3.io";
-}
-
-function gotoGitHub() {
-  window.location.href = "//github.com/CircuitCoder";
-}
-
-function gotoBilibili() {
-  window.location.href = "//space.bilibili.com/523981";
-}
